586f7f5d58252b5a488b8f758f53aeb487a89f44
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-06 02:02:40 +0900

    feat: 보호자와 연결된 장애인 목록 조회

--- controllers/matching.controller.js
+++ controllers/matching.controller.js
@@ -1,7 +1,36 @@
 import { StatusCodes } from 'http-status-codes';
 
-import { addMatching } from '../services/matching.service.js';
+import { addMatching, getRelatedDisabledList } from '../services/matching.service.js';
 import { InvalidInputError } from '../utils/errors/errors.js';
+
+export const getRelatedDisabledListController = async (req, res, next) => {
+  /*
+    1. 로그인 된 사용자의 id를 disabled_profile / assistant_id에서 조회
+    2. 해당 정보를 기반으로 찾은 user_id를 기반으로 user & disabled_profile & disabled_type join 후 disabled_user_id 조회
+    3. 필요 항목
+    1. birthdate 토대 나이 계산
+    2. residence_area
+    3. disabled_type_id와 disability_level, description 조회
+  */
+  try {
+    const authorId = req?.user?.userId;
+    if (!authorId) {
+      throw new InvalidInputError('userId가 올바르지 않습니다.');
+    }
+
+    const relatedDisabledList = await getRelatedDisabledList(authorId);
+
+    if (!relatedDisabledList || relatedDisabledList.length === 0) {
+      throw new InvalidInputError('연결된 장애인 프로필을 찾을 수 없습니다.');
+    }
+
+    return res.success({
+      relatedDisabledList,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
 
 // 도움 요청 등록
 export const handleAddMatching = async (req, res, next) => {
--- routes/matching.router.js
+++ routes/matching.router.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { handleAddMatching } from '../controllers/matching.controller.js';
+import { getRelatedDisabledListController, handleAddMatching } from '../controllers/matching.controller.js';
 import { authenticateAccessToken } from '../middleware/authenticate.jwt.js';
 
 const router = Router();
 
+router.get("/connected-disabled-user", authenticateAccessToken, getRelatedDisabledListController)
 router.post('/', authenticateAccessToken, handleAddMatching);
 
 export default router;
