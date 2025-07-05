0e6cb884824004f3644b1ad4095eb8c047875808
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-06 00:19:08 +0900

    refactor: login/register code service로의 분리

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,22 +1,13 @@
 import prisma from '../prisma/db.js';
-import { registerUserService } from '../services/auth.service.js';
+import { changeDateStringToISO, getUserByPhoneNumber, registerUserService } from '../services/auth.service.js';
 import { createAccessToken } from '../utils/create.jwt.tokens.js';
 import { InvalidInputError } from '../utils/errors/errors.js';
 
 export const registerController = async (req, res, next) => {
   try {
-    let { profileImage, phoneNumber, password, isDisabled, name, birthdate, residenceArea } =
+    req.body.birthdate = changeDateStringToISO(req.body.birthdate);
+    const { profileImage, phoneNumber, password, isDisabled, name, birthdate, residenceArea } =
       req.body;
-
-    if (birthdate) {
-      if (typeof birthdate === 'string') {
-        const parsed = new Date(birthdate);
-        if (isNaN(parsed.getTime())) {
-          throw new InvalidInputError('유효하지 않은 생년월일 형식입니다.');
-        }
-        birthdate = parsed.toISOString();
-      }
-    }
 
     await registerUserService({
       profileImage,
@@ -39,17 +30,24 @@ export const registerController = async (req, res, next) => {
 export const loginController = async (req, res, next) => {
   try {
     const { phoneNumber, password } = req.body;
-    const user = await prisma.user.findUnique({
-      where: { phoneNumber },
-    });
+    const user = await getUserByPhoneNumber(phoneNumber);
 
-    if (!user || user.password !== password) {
+    if (!user) {
+      throw new InvalidInputError('전화번호 또는 비밀번호가 올바르지 않습니다.');
+    }
+
+    if (user.password !== password) { // 해커톤이라 그냥 평문 비교
+      console.log(user);
+      console.error('로그인 실패: 전화번호 또는 비밀번호가 올바르지 않습니다.', {
+        phoneNumber,
+        password,
+        user_password: user.password
+      });
       throw new InvalidInputError('전화번호 또는 비밀번호가 올바르지 않습니다.');
     }
 
     return res.success({
       message: '로그인에 성공하였습니다.',
-      user,
       accessToken: createAccessToken({ userId: user.userId }),
     });
   } catch (error) {
--- services/auth.service.js
+++ services/auth.service.js
@@ -1,7 +1,30 @@
 import prisma from '../prisma/db.js';
+import { AlreadyExistsError } from '../utils/errors/errors.js';
 
 export const registerUserService = async (data) => {
-  return await prisma.user.create({
-    data,
+  try {
+    await prisma.user.create({
+      data,
+    });
+  } catch (error) {
+    throw new AlreadyExistsError('회원가입에 실패하였습니다. 중복된 전화번호는 아닌지 확인해주세요.');
+  }
+};
+
+export const changeDateStringToISO = (dateString) => {
+  if (typeof dateString === 'string') {
+    const parsedDate = new Date(dateString);
+    if (isNaN(parsedDate.getTime())) {
+      throw new Error('유효하지 않은 날짜 형식입니다.');
+    }
+    return parsedDate.toISOString();
+  }
+  return dateString;
+}
+
+export const getUserByPhoneNumber = async (phoneNumber) => {
+  return await prisma.user.findUnique({
+    where: { phoneNumber },
   });
 };
+
