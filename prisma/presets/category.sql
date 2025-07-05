fd54785fdcc3e0e308776178e50740cedbf6c901
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-06 00:05:42 +0900

    feat: add category/type preset for db

---
+++ prisma/presets/category.sql
@@ -0,0 +1,15 @@
+INSERT INTO matching_category (name, description)
+VALUES
+    ('외출 및 이동', NULL),
+    ('일상 생활', NULL),
+    ('주거 환경', NULL),
+    ('취미 및 사회활동', NULL),
+    ('소통 및 케어', NULL);
+
+INSERT INTO disabled_type (name, description)
+VALUES
+    ('상지(팔)', NULL),
+    ('하지(다리)', NULL),
+    ('척추', NULL),
+    ('몸통', NULL),
+    ('전신', NULL);
