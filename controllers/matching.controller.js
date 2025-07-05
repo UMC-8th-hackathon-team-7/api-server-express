b0d44f36b07cf18967c0c730ee0811117989ec5d
dadang6842 <dadang6842@gmail.com>
2025-07-06 01:39:18 +0900

    feat: 도움 전체 리스트 보기

--- controllers/matching.controller.js
+++ controllers/matching.controller.js
@@ -1,6 +1,6 @@
 import { StatusCodes } from 'http-status-codes';
 
-import { addMatching } from '../services/matching.service.js';
+import { addMatching, listMatching } from '../services/matching.service.js';
 import { InvalidInputError } from '../utils/errors/errors.js';
 
 // 도움 요청 등록
@@ -27,6 +27,25 @@ export const handleAddMatching = async (req, res, next) => {
     const matching = await addMatching(data);
 
     return res.status(StatusCodes.OK).success(matching);
+  } catch (error) {
+    next(error);
+  }
+};
+
+// 도움 전체 보기
+export const handlelistMatching = async (req, res, next) => {
+  try {
+    const authorId = req?.user?.userId;
+
+    if (!authorId) {
+      throw new InvalidInputError('userId가 올바르지 않습니다.');
+    }
+
+    const categoryId = parseInt(req.query.category, 10);
+
+    const list = await listMatching(categoryId);
+
+    return res.status(StatusCodes.OK).success(list);
   } catch (error) {
     next(error);
   }
