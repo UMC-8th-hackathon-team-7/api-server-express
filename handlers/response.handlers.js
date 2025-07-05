efc23b0bcde78aedacd21c07c7f92d62a65c5013
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-07-05 22:47:19 +0900

    fix: DB 내 bigint -> int 변경 (#8)
    * config: add prisma script to dev
    
    * feat: JWT verification middlewares
    
    test code 와 함꼐 추가하였습니다.
    JWT token을 임의 생성하는 router 또한 추가.
    
    * feat: table bigint -> int 변경
    
    bigint type가 json으로 serialize 되지 않는 issue로 변경처리

--- handlers/response.handlers.js
+++ handlers/response.handlers.js
@@ -1,5 +1,20 @@
 import { logError } from "./error.logger.js";
 
+// function safeJsonValue(value) {
+//   if (Array.isArray(value)) {
+//     return value.map(safeJsonValue)
+//   }
+//   if (value && typeof value === 'object') {
+//     return Object.fromEntries(
+//       Object.entries(value).map(([k, v]) => [k, safeJsonValue(v)])
+//     )
+//   }
+//   if (typeof value === 'bigint') {
+//     return value.toString()
+//   }
+//   return value
+// }
+
 export const responseHandler = (req, res, next) => {
   res.success = (success) =>
     res.json({ resultType: "SUCCESS", error: null, success });
