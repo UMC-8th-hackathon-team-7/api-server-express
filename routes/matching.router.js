218c6adbcee1444682c35a24fd102588753473ae
Kyeoungwoon Park <saveearth1@cau.ac.kr>
2025-07-06 02:05:00 +0900

    feat: 보호자와 연결된 장애인 목록 조회 기능 (#31)
    * chore: delete unused codes
    
    * db: matching table timestamps & change assistant_id to optional
    
    * feat: 장애인 회원가입 구현
    
    * feat: 보호자와 연결된 장애인 목록 조회

--- routes/matching.router.js
+++ routes/matching.router.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 
-import { handleAddMatching, handlelistMatching } from '../controllers/matching.controller.js';
+import { handleAddMatching, handlelistMatching, getRelatedDisabledListController } from '../controllers/matching.controller.js';
 import { authenticateAccessToken } from '../middleware/authenticate.jwt.js';
 
 const router = Router();
 
+router.get("/connected-disabled-user", authenticateAccessToken, getRelatedDisabledListController)
 router.post('/', authenticateAccessToken, handleAddMatching);
 router.get('/', authenticateAccessToken, handlelistMatching);
 
