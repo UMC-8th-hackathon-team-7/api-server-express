e2d06af1fb4de8f1b6774fd4ffabf939f6a2c0c8
kyeoungwoon <saveearth1@cau.ac.kr>
2025-07-05 20:51:53 +0900

    config: add prisma client to gitignore

--- routes/routes.index.js
+++ routes/routes.index.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import testRouter from "./test.router.js"; // Importing the test router
 
 const routers = Router();
 
 routers.all("/ping", (req, res) => res.status(200).send("Pong!"));
+
+routers.use("/test", testRouter)
 
 export default routers;
